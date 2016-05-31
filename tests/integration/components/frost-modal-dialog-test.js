bcddeeac6a414c80d7d2860336c76079ec697a98
Roxanne Panthaky <rpanthak@ciena.com>
2016-05-31 11:11:02 -0400

    fix up tests

--- tests/integration/components/frost-modal-dialog-test.js
+++ tests/integration/components/frost-modal-dialog-test.js
@@ -1,11 +1,13 @@
 // import Ember from 'ember'
-import { expect, assert } from 'chai'
+import { expect } from 'chai'
 import {
   describeComponent,
   it
 } from 'ember-mocha'
 import hbs from 'htmlbars-inline-precompile'
 import { initialize } from 'ember-block-slots/initializers/component-block-slots'
+import $ from 'jquery'
+import {beforeEach} from 'mocha'
 
 describeComponent(
   'frost-modal-dialog',
@@ -14,8 +16,20 @@ describeComponent(
     integration: true
   },
   function () {
+    let container, application
+
+    beforeEach(function () {
+      Ember.testing = true
+      Ember.run(() => {
+        application = Ember.Application.create()
+        container = application.__container__
+        application.deferReadiness()
+      })
+      initialize(container, application)
+    })
+
     it('opens frost-modal-dialog of type ("confirmation")', function () {
-      this.on('confirm', function() {
+      this.on('confirm', function () {
       })
       this.render(hbs`{{#frost-modal-dialog
         title='confirmation'
@@ -33,10 +47,10 @@ describeComponent(
       {{/frost-modal-dialog}}`)
 
       this.$('.frost-button')[0].click()
-        let length = $('[data-test-id="modalWindow"].remodal-is-opened').length ||
-                    $('[data-test-id="modalWindow"].remodal-is-opening').length
-        expect(length).to.equal(1)
-        expect($('[data-test-id="yielded"] .header').text()).to.have.string('confirmation')
+      let length = $('[data-test-id="modalWindow"].remodal-is-opened').length ||
+                  $('[data-test-id="modalWindow"].remodal-is-opening').length
+      expect(length).to.equal(1)
+      expect($('[data-test-id="yielded"] .header').text()).to.have.string('confirmation')
     })
   }
 )

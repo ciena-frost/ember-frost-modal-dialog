863606f77af90ed9c79179ff3d59ed03740bc16f
Roxanne Panthaky <rpanthak@ciena.com>
2016-06-01 16:20:39 -0400

    clean up space

--- addon/components/frost-modal-dialog.js
+++ addon/components/frost-modal-dialog.js
@@ -5,7 +5,7 @@ import {PropTypes} from 'ember-prop-types'
 
 const {Component, assert} = Ember
 
-export default Component.extend( {
+export default Component.extend({
   tagName: 'frost-modal-dialog',
   classNames: ['frost-modal-dialog'],
 
